6420a5ac0dbc2de2ffc39892ab2d7cdb0dcc13b4
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-01-31 15:21:14 -0600

    node-1-aftn finished

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { json } = require("body-parser");
+const mc = require(__dirname + "/controllers/messages_controller");
+
+const app = express();
+
+app.use(json());
+app.use(express.static(__dirname + "/../public/build"));
+
+const messagesBaseUrl = "/api/messages";
+
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.destroy);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
